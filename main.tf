cbced477bf031737ae452800c827c50ce38641ed
Ibarra Leonel Christian <cibarraleonel@gmail.com>
2025-07-29 18:19:35 -0300

    añado gateway y rpute table

--- main.tf
+++ main.tf
@@ -95,3 +95,25 @@ resource "aws_subnet" "main_subnet" {
     Name = "redmine-subnet"
   }
 }
+
+# Gatewat internet
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+#Route table con internet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
