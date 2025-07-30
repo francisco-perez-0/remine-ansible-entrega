49c8e506b99330835236c667757dec686e247f2d
Ibarra Leonel Christian <cibarraleonel@gmail.com>
2025-07-30 11:09:39 -0300

    Se crea logica para rds

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+#ip publica para conexion
 output "instance_ip" {
   value = aws_instance.redmine.public_ip
+}
+
+# endpoint para conexion
+output "rds_endpoint" {
+  value = aws_db_instance.redmine.endpoint
 }
--- variables.tf
+++ variables.tf
@@ -13,3 +13,15 @@ variable "public_key_path" {
   description = "Ruta de clave pública local"
   type        = string
 }
+
+
+
+# VARIABLES BASE DE DATOS
+variable "db_name"     {default = "redmine_production" }
+variable "db_user"     { default = "redmine" }
+variable "db_password" { sensitive = true }
+variable "db_port"     { default = 3306 }
+variable "db_instance_class" { default = "db.t3.micro" }
+variable "db_allocated_storage" { default = 20 }
+variable "subnet_ids" { type = list(string) }
+variable "vpc_id" { type = string }
